f1ec7d4fbeca6e7bebba3c0140cfb810945b3f12
arnaldo-dotcom <arnaldo@moveo.ai>
2026-06-27 13:35:06 -0300

    migrar para upstash redis

--- api/_db.ts
+++ api/_db.ts
@@ -1,4 +1,9 @@
-import { kv } from "@vercel/kv";
+import { Redis } from "@upstash/redis";
+
+const kv = new Redis({
+  url: process.env.KV_REST_API_URL!,
+  token: process.env.KV_REST_API_TOKEN!,
+});
 import type { Cliente } from "./_types";
 
 export async function getClienteByCpf(cpf: string): Promise<Cliente | null> {
